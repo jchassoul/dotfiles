744abffcb28fe03240ee5900c271c2d4241af503
Jean Chassoul <jchassoul@gmail.com>
2021-06-21 08:49:19 -0600

    clean, clean, clean

--- install/winetricks.sh
+++ install/winetricks.sh
@@ -4,13 +4,18 @@
 
 rm -rf ~/.wine
 
+mkdir -p ~/.local/bin
+
 cd ~/.local/bin
 
 wget -nc https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks
 
-chmod +X winetricks
+chmod +x winetricks
 
 WINEARCH=win32 wineboot
+
 winetricks -q vcrun2012
+
 winetricks -q vcrun2013
+
 winetricks -q dxvk
