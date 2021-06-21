15a3a864355e8e65e8cd3316d61f13f95adce795
Jean Chassoul <jchassoul@gmail.com>
2021-06-20 18:47:23 -0600

    clean, clean, clean

--- install/wine.sh
+++ install/wine.sh
@@ -2,6 +2,8 @@
 
 dpkg --add-architecture i386
 
+apt update
+
 apt -y install --install-recommends libgnutls30:i386 libldap-2.4-2:i386\
     libgpg-error0:i386 libxml2:i386 libasound2-plugins:i386 libsdl2-2.0-0:i386\
     libsdl2-2.0-0 libavutil56 libavcodec58 libavutil56:i386 libavcodec58:i386\
@@ -9,10 +11,12 @@ apt -y install --install-recommends libgnutls30:i386 libldap-2.4-2:i386\
     libgl1-mesa-dri:i386 libstb0 libstb0:i386 mesa-vulkan-drivers xvfb libc-bin\
     mesa-vulkan-drivers:i386
 
-wget -nc https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/Debian_10/amd64/libfaudio0_20.01-0~buster_amd64.deb
-wget -nc https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/Debian_10/i386/libfaudio0_20.01-0~buster_i386.deb
-dpkg -i libfaudio0_20.01-0~buster_amd64.deb
-dpkg -i libfaudio0_20.01-0~buster_i386.deb
+# libgaudio from backports!
+
+# missing apt update
+
+# missing apt --fix-broken install
+
 
 wget -nc https://dl.winehq.org/wine-builds/debian/dists/buster/main/binary-i386/winehq-staging_5.13~buster_i386.deb
 wget -nc https://dl.winehq.org/wine-builds/debian/dists/buster/main/binary-i386/wine-staging-i386_5.13~buster_i386.deb
@@ -26,3 +30,12 @@ wget -nc https://dl.winehq.org/wine-builds/debian/dists/buster/main/binary-amd64
 dpkg -i wine-staging_5.13~buster_i386.deb
 dpkg -i wine-staging-i386_5.13~buster_i386.deb
 dpkg -i winehq-staging_5.13~buster_i386.deb
+
+
+apt --fix-broken install
+
+dpkg -i wine-staging_5.13~buster_amd64.deb
+dpkg -i wine-staging-amd64_5.13~buster_amd64.deb
+dpkg -i winehq-staging_5.13~buster_amd64.deb
+
+apt --fix-broken install
