98c3ee4d968405e7d12db70baa716b4128805563
Jean Chassoul <jchassoul@gmail.com>
2024-11-13 05:41:43 -0600

    clean, clean, clean

---
+++ config/wezterm/wezterm.lua
@@ -0,0 +1,15 @@
+local wezterm = require("wezterm")
+
+return {
+    hide_tab_bar_if_only_one_tab = true,
+    enable_tab_bar = false,
+    font_size = 12.0,
+    font = wezterm.font("Inconsolata Nerd Font Mono"),
+    window_background_opacity = 0.97,
+    exit_behavior="Close",
+    window_close_confirmation = 'NeverPrompt',
+    default_cursor_style = "BlinkingBlock",
+    cursor_blink_rate = 500,
+    enable_wayland = false,
+
+}
